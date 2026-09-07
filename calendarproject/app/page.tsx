1fe5387768240929a44d29e2fd47291039cfd8a9
MPL <sistemas.co@gmail.com>
2026-09-07 22:34:49 +0200

    La página seguirá siendo pública para usuarios no autenticados, mientras que los usuarios autenticados serán enviados al calendario

--- calendarproject/app/page.tsx
+++ calendarproject/app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/calendar");
+  }
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
